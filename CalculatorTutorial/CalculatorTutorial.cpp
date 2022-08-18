6cb0fcc0e21d1825a6ee7be2c01d3f01ac7e3a59
Hussain <Hussain@DESKTOP-HVM3VAQ>
2022-08-18 19:45:03 +0530

    Add project files.

---
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -0,0 +1,35 @@
+//CPP app called Calculator
+
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+	double x = 0.0;
+	double y = 0.0;
+	double result = 0.0;
+	char oper = '+';
+
+	cout << "Calculator Console Application" << endl << endl;
+	cout << "Please enter the operation to perform. Formate: |a+b| |a-b| |a*b| |a/b|" << endl;
+	
+	Calculator c;
+	while (true)
+	{
+		cin >> x >> oper >> y;
+		if (oper == '/' && y == 0)
+		{
+			cout << "Division by 0 exception" << endl;
+			continue;
+		}
+		else
+		{
+			result = c.Calculate(x, oper, y);
+		}
+		cout << "Result is: " << result << endl;
+	}
+
+	return 0;
+}
